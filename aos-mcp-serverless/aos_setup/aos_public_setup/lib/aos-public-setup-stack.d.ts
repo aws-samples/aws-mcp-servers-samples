551496c52a48243cd22c790de3b51e50ff9070b7
Ubuntu <ubuntu@ip-172-31-30-139.us-west-2.compute.internal>
2025-09-07 03:43:47 +0000

    archive

--- aos-mcp-serverless/aos_setup/aos_public_setup/lib/aos-public-setup-stack.d.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-export interface AosPublicSetupStackProps extends cdk.StackProps {
-    openSearchUsername: string;
-    openSearchPassword: string;
-}
-export declare class AosPublicSetupStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: AosPublicSetupStackProps);
-}
